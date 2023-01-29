3e003c4bc7c8b5c73317ef67e26789c4668a3f1c
mhduusop <mhduusop@gmail.com>
2023-01-30 00:20:04 +0700

    first commit

---
+++ src/components/CardBook.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="d-flex justify-content-center">
+        <a href="#" class="card-wrap">
+            <div class="card">
+                <img src="https://cdn.lorem.space/images/movie/.cache/150x220/godzilla-kong-2.jpg" class="card-img card-cover" alt="...">
+            </div>
+            <h5 class="card-title">Godzilla vs Kong</h5>
+            <p class="card-text">Author</p>
+        </a>
+    </main>
+</template>
+
+<style scoped>
+.card-wrap{
+    padding: 5px;
+}
+
+.card {
+    width: 11rem;
+    border: none
+}
+
+.card-cover {
+    width: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CardBook from '../components/CardBook.vue';
+</script>
+
+<template>
+  <div class="container-fluid">
+    <main>
+      <CardBook />
+    </main>
+  </div>
+</template>
