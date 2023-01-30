8cb20f8eafa0244a73fc1953e66c43597146a215
mhduusop <mhduusop@gmail.com>
2023-01-31 01:22:18 +0700

    add: navbar and fixing card

--- src/components/CardBook.vue
+++ src/components/CardBook.vue
@@ -1,26 +1,88 @@
 <template>
     <main class="d-flex justify-content-center">
-        <a href="#" class="card-wrap">
+        <a href="#" class="card-wrap text-decoration-none" v-for="card in 10" :key="card">
             <div class="card">
-                <img src="https://cdn.lorem.space/images/movie/.cache/150x220/godzilla-kong-2.jpg" class="card-img card-cover" alt="...">
+                <img src="https://api.lorem.space/image/book" class="card-img card-cover" alt="...">
             </div>
-            <h5 class="card-title">Godzilla vs Kong</h5>
-            <p class="card-text">Author</p>
+            <div class="title mt-2">
+                <h5 class="card-title">Godzilla vs Kong</h5>
+                <p class="card-title-author">Author</p>
+            </div>
+        </a>
+        <a href="#" class="card-wrap text-decoration-none" v-for="card in 10" :key="card">
+            <div class="card">
+                <img src="https://api.lorem.space/image/movie" class="card-img card-cover" alt="...">
+            </div>
+            <div class="title mt-2">
+                <h5 class="card-title">Godzilla vs Kong</h5>
+                <p class="card-title-author">Author</p>
+            </div>
+        </a>
+        <a href="#" class="card-wrap text-decoration-none" v-for="card in 10" :key="card">
+            <div class="card">
+                <img src="https://api.lorem.space/image/game" class="card-img card-cover" alt="...">
+            </div>
+            <div class="title mt-2">
+                <h5 class="card-title">Godzilla vs Kong</h5>
+                <p class="card-title-author">Author</p>
+            </div>
         </a>
     </main>
 </template>
 
+<script>
+// import { ref, onMounted } from 'vue'
+// import axios from "axios";
+
+// const img = ref("")
+
+// function randomImg() {
+//     axios.get('https://api.lorem.space/image/book', {
+//         headers: { "Access-Control-Allow-Origin": "*" }
+//     }).then(res => img.value = res.data)
+// }
+
+// randomImg();
+
+</script>
+
 <style scoped>
-.card-wrap{
+main {
+    margin: auto;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    max-width: 1884px;
+}
+
+.card-wrap:hover {
+    color: #ECB365;
+}
+
+.card-title-author {
+    color: #fff;
+    font-weight: 100;
+}
+
+.card-wrap {
     padding: 5px;
+    float: none;
+    margin-bottom: 10px;
+    color: #fff;
 }
 
 .card {
     width: 11rem;
-    border: none
+    border: none;
+    margin: 0 auto;
+    transition: transform 250ms;
 }
 
 .card-cover {
     width: 100%;
+}
+
+.card:hover {
+    transform: translateY(-10px);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup>
-import CardBook from '../components/CardBook.vue';
+import CardBook from "../components/CardBook.vue";
+import NavbarComponent from "../components/NavbarComponent.vue";
 </script>
 
 <template>
-  <div class="container-fluid">
-    <main>
-      <CardBook />
-    </main>
-  </div>
+	<div class="container-fluid">
+		<header>
+			<NavbarComponent />
+		</header>
+		<main>
+			<div class="mt-4">
+				<CardBook />
+			</div>
+		</main>
+	</div>
 </template>
