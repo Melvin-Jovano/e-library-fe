115259409baeccdc1d7bc1f26e61ae49e80ff977
KevinFrederick <kevin55622@gmail.com>
2023-01-31 22:17:25 +0700

    Book By Id template

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BookDetail from '../components/BookDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/books/:bookId',
+      name: 'details',
+      component: BookDetail,
+      props: true
+    }
   ]
 })
 
