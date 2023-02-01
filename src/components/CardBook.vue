bdb7c6b4dd3dca26c146e8ca215b3bf43c98be44
mhduusop <mhduusop@gmail.com>
2023-02-01 23:10:30 +0700

    add router-link

--- src/components/CardBook.vue
+++ src/components/CardBook.vue
@@ -1,34 +1,46 @@
 <template>
     <main class="d-flex justify-content-center">
-        <a href="#" class="card-wrap text-decoration-none" v-for="card in 10" :key="card">
-            <div class="card">
-                <img src="https://api.lorem.space/image/book" class="card-img card-cover" alt="...">
-            </div>
-            <div class="title mt-2">
-                <h6 class="card-title text-center">Godzilla vs Kong</h6>
-                <p class="card-title-author text-center">Author</p>
-            </div>
-        </a>
-        <a href="#" class="card-wrap text-decoration-none" v-for="card in 10" :key="card">
-            <div class="card">
-                <img src="https://api.lorem.space/image/movie" class="card-img card-cover" alt="...">
-            </div>
-            <div class="title mt-2">
-                <h6 class="card-title text-center">Godzilla vs Kong</h6>
-                <p class="card-title-author text-center">Author</p>
-            </div>
-        </a>
-        <a href="#" class="card-wrap text-decoration-none" v-for="card in 10" :key="card">
-            <div class="card">
-                <img src="https://api.lorem.space/image/game" class="card-img card-cover" alt="...">
-            </div>
-            <div class="title mt-2">
-                <h6 class="card-title text-center">Godzilla vs Kong</h6>
-                <p class="card-title-author text-center">Author</p>
-            </div>
-        </a>
+        <div class="card-wrap" v-for="book in bookData" :key="book">
+            <router-link class="text-decoration-none" :to="{ name: 'details', params: { bookId: book.id } }">
+                <div class="card">
+                    <img :src="API_URL + book.cover" class="card-img card-cover" alt="...">
+                </div>
+                <div class="title mt-3">
+                    <h6 class="card-title text-center">{{ book.title }}</h6>
+                    <p class="card-title-author text-center">{{ book.author.name }}</p>
+                </div>
+            </router-link>
+        </div>
     </main>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { getAllBook } from '../api/books.js';
+import { API_URL } from '../const.js';
+
+const lastPostId = ref(null);
+const limit = ref(5);
+const bookData = ref([])
+
+console.log(bookData);
+
+async function getAllBookData() {
+    try {
+        const allBook = await getAllBook({ limit: limit.value })
+        if (allBook.data.message === 'SUCCESS') {
+            bookData.value = allBook.data.data.data;
+            lastPostId.value = getContents.data.data.lastId;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+onMounted(async () => {
+    await getAllBookData();
+});
+</script>
 
 <style scoped>
 main {
@@ -38,8 +50,12 @@ main {
     flex-wrap: wrap;
 }
 
-.card-wrap:hover {
-    color: #ECB365;
+.text-decoration-none:hover {
+    color: #fbc100;
+}
+
+.text-decoration-none {
+    color: #fff;
 }
 
 .card-title-author {
@@ -52,7 +68,6 @@ main {
     padding: 5px;
     float: none;
     margin-bottom: 10px;
-    color: #fff;
 }
 
 .card {
